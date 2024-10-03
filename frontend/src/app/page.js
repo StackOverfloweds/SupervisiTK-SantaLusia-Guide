c1d52c881159179801817d9c170ec488e2cf5873
abdisavia <ridhodanabdi7a@gmail.com>
2024-10-03 20:44:40 +0700

    'keunggulan sekolah' section finished, working on 'CCUP' section

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,7 +2,10 @@ import Image from "next/image";
 import Fotosantalusia from "@/img/FOTOSANTALUSIA.jpg"
 import iconBuku from "@/img/iconBuku.jpg"
 import santaLusia from "@/img/santaLusia.jpg"
+import GuruSantaLusia from "@/img/Gurusantalusia.jpg"
 import NavbarUser from "@/components/ui/NavbarUser"
+import { FaRegGem } from "react-icons/fa";
+import { GoTrophy } from "react-icons/go";
 
 export default function Home() {
   return (
@@ -68,9 +71,66 @@ export default function Home() {
 
       {/* preschool education */}
       <section>
-        <div className="py-5 px-56">
-          <p className="font-bebasNeue text-center w-full text-2xl text-gray-400 font-thin">tentang kami</p>
+        <div className="pt-5 px-56">
+          <p className="font-bebasNeue text-center w-full text-2xl text-gray-400 font-thin uppercase">tentang kami</p>
           <h1 className="font-poppinsBold font-black text-3xl text-gray-800 text-center">Best Pre School Education!</h1>
+          <div className="grid grid-cols-4 grid-rows-4 gap-3 w-full mt-5 p-5 h-[40em]">
+            <div className=" text-center font-poppins text-gray-500 row-span-2 m-auto">
+              <FaRegGem size={50} color="white" className="p-2 rounded-full bg-pink-500 mx-auto mb-2"/>
+              <h1 className="mb-2">MOTTO KAMI</h1>
+              <span>
+                <p className="text-gray-800 font-medium italic font-poppinsBold">“CERDAS, CERIA, UNGGUL DAN PEDULI SESAMA (CCUP)"</p>
+              </span>
+            </div>
+            <div className="text-center font-poppins text-gray-500 row-span-2 m-auto">
+              <GoTrophy size={50} color="white" className="p-2 rounded-full bg-pink-500 mx-auto mb-2"/>
+              <h1 className="mb-2">VISI KAMI</h1>
+              <span className="">
+                <p className="text-gray-800 font-medium italic font-poppinsBold">“Menjadi Lembaga Pendidikan yang Unggul dan Berkualitas dengan Semangat Santa Lusia”</p>
+              </span>
+            </div>
+            <div className="border col-span-2 row-span-4 relative">
+              <Image src={GuruSantaLusia} fill={true}></Image>
+            </div>
+            <div className="border row-span-2 relative">
+              <Image src={GuruSantaLusia} fill={true} className="object-cover"></Image>
+            </div>
+            <div className="border row-span-2 relative">
+              <Image src={GuruSantaLusia} fill={true} className="object-cover"></Image>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Keunggulan Sekolah Section */}
+      <section className="bg-[#feca02] px-56 pt-5">
+        <div className="grid grid-rows-6 items-center">
+          <div className="font-poppins">
+            <p className="font-bebasNeue text-2xl text-center">KEUNGGULAN SEKOLAH KAMI</p>
+            <h1 className="font-poppinsBold text-3xl text-center">Layanan yang Kami Sediakan</h1>
+          </div>
+          <div className="grid grid-cols-4 gap-3 row-span-5 pt-5">
+            <div className="bg-white h-full p-5 text-center">
+              <div className="bg-gray-500 rounded-full h-24 w-24 mx-auto"></div>
+              <h1 className="text-pink-500 font-bebasNeue text-2xl my-2">fasilitas yang sangat baik</h1>
+              <p className="text-gray-600 font-poppins text-sm italic ">Sekolah Santa Lusia menyediakan ruang kelas yang ceria dan aman, lengkap dengan alat bermain edukatif yang menarik untuk mendukung pembelajaran anak-anak TK. Kami juga memiliki taman bermain yang luas dan nyaman, di mana anak-anak dapat bermain dan belajar dengan gembira setiap hari.</p>
+            </div>
+            <div className="bg-white h-full p-5 text-center">
+              <div className="bg-gray-500 rounded-full h-24 w-24 mx-auto"></div>
+              <h1 className="text-[#488df4] #488df4 font-bebasNeue text-2xl my-2">PEMBELAJARAN DAN PENUMBUHAN IMAN ANAK</h1>
+              <p className="text-gray-600 font-poppins text-sm italic ">Kami menanamkan nilai-nilai iman sejak dini melalui cerita-cerita inspiratif dan kegiatan rohani yang sederhana, membimbing anak-anak untuk tumbuh dengan hati yang penuh kasih dan kebaikan. Di Sekolah Santa Lusia, pembelajaran anak TK dirancang secara menyenangkan dan interaktif, sehingga anak-anak dapat mengembangkan kreativitas dan rasa ingin tahu mereka.</p>
+            </div>
+            <div className="bg-white h-full p-5 text-center">
+              <div className="bg-gray-500 rounded-full h-24 w-24 mx-auto"></div>
+              <h1 className="text-[#3cc4c4] font-bebasNeue text-2xl my-2">Pembelajaran yang kreatif</h1>
+              <p className="text-gray-600 font-poppins text-sm italic ">Di Sekolah Santa Lusia, pembelajaran anak TK dirancang secara menyenangkan dan interaktif, memanfaatkan alat bermain edukatif yang kreatif. Metode pembelajaran kami mendorong anak-anak untuk bereksplorasi, berkreasi, dan mengembangkan rasa ingin tahu mereka secara alami.</p>
+            </div>
+            <div className="bg-white h-full p-5 text-center">
+              <div className="bg-gray-500 rounded-full h-24 w-24 mx-auto"></div>
+              <h1 className="text-[#feca02] font-bebasNeue text-2xl my-2">GURU GURU YANG KOMPETEN</h1>
+              <p className="text-gray-600 font-poppins text-sm italic ">Guru-guru di Sekolah Santa Lusia adalah tenaga pengajar yang kompeten dan berpengalaman, selalu siap membimbing dan menginspirasi setiap anak. Dengan pendekatan yang penuh kasih dan metode pengajaran yang inovatif, mereka memastikan setiap siswa mendapatkan perhatian dan pendidikan terbaik.</p>
+            </div>
+          </div>
         </div>
       </section>
     </div>
