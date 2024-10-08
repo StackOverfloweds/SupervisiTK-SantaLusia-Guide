50d19fa76cefb440303ec68505a3b5efdbece8c3
StackOverfloweds <bryangithub32@gmail.com>
2024-10-08 22:59:06 +0700

    update add conf for migration

--- server/database/migrations/0001_01_01_000000_create_users_table.php
+++ server/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,14 +12,27 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->uuid('user_id')->primary();
+            $table->string('name')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role',['wali_murid','guru','kepala_sekolah']);
             $table->timestamps();
         });
+        
+        Schema::create('profile_user', function (Blueprint $table) {
+            $table->uuid('profile_id');
+            $table->uuid('user_id');
+            $table->string('email')->unique();
+            $table->string('')->uphone_numbernique();
+            $table->text('second_phone_number')->nullable()->unique();
+            $table->text('address')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('user_id')
+                ->on('users')
+                ->onDelete('cascade');
+        }); 
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
