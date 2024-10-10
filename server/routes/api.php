ecfedd311c43c7a73c2d834f8ba7fd1a4b48a757
StackOverfloweds <bryangithub32@gmail.com>
2024-10-10 16:45:20 +0700

    update: add cors origin

--- server/routes/api.php
+++ server/routes/api.php
@@ -5,13 +5,20 @@ use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('Auth')->group(function () {
-    // Registration Route (no middleware)
-    Route::post('/register', [RegisterController::class, 'register']);
 
-    // Login Route with middleware to check token
-    Route::post('/login', action: [LoginController::class, 'login'])->middleware('token');;
+Route::middleware(['cors'])->group(function () {
 
-    // Logout Route with middleware to check token
-    Route::delete('/logout', [LogoutController::class, 'logout']);
+    
+    Route::prefix('Auth')->group(function () {
+        // Registration Route (no middleware)
+        Route::post('/register', [RegisterController::class, 'register']);
+        
+        // Login Route with middleware to check token
+        Route::post('/login', action: [LoginController::class, 'login'])->middleware('token');;
+        
+        // Logout Route with middleware to check token
+        Route::delete('/logout', [LogoutController::class, 'logout']);
+    });
+
+    
 });
